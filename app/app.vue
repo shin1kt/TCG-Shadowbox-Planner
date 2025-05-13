52860955023919375c2adcc57c74f4e6b3ca07de
shin1kt <nomeshi74@gmail.com>
2025-05-13 10:51:11 +0900

    feat: nuxt + vuetify setup

---
+++ app/app.vue
@@ -0,0 +1,7 @@
+<template>
+  <NuxtLayout>
+    <v-app class="pa-10">
+      <NuxtPage />
+    </v-app>
+  </NuxtLayout>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <v-card>
+    <v-card-title>タイトルテスト</v-card-title>
+    <v-card-text>内容</v-card-text>
+  </v-card>
+</template>
